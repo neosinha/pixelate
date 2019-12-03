759bcad76e96e79f74878f16531b6ff685fb2b09
Navendu Sinha <navendusinha@icloud.com>
2019-12-03 00:37:57 -0800

    More changes to get application running.

--- ui_www/js/app.js
+++ ui_www/js/app.js
@@ -8,10 +8,10 @@ function appInit() {
 	appNavBar();
 	loadLandingView(); 
 	updatePerSecond();
+	//fileUploadRow();
 	addImageCol();
 	addButtonCol();
-
-	initUpload();
+	//initUpload();
 }
 
 var serialnum = null; 
@@ -28,28 +28,6 @@ function updatePerSecond() {
 	setTimeout(updatePerSecond, 1000);
 }
 
-function initUpload() {
-    var jxhr = $('#imgbtn').fileupload({
-        dataType: 'image',
-        done: function (e, data) {
-            $.each(data.files, function (index, file) {
-                alert("Fileupload...");
-                $('<p/>').text(file.name).appendTo('#files');
-            });
-        },
-        progressall: function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
-    }
-    }).bind('fileuploadcompleted',
-            function (e, data) {
-                alert("Message","Title")
-               }
-           );
-}
 
 
 function loadLandingView() {
@@ -64,16 +42,13 @@ function loadLandingView() {
 		var resultarea = ui.createElement('div', 'results');
 		var notifyarea = ui.createElement('div', 'notify');
 		
-		//jum.appendChild(farStatusForm());
-        //jum.appendChild(guagearea);
-        jum.appendChild(brow);
+
         jum.appendChild(crow);
+        jum.appendChild(brow);
 		jum.appendChild(resultarea);
 		
 		//jum.appendChild(xmlarea);
 		jum.appendChild(notifyarea);
-		
-		
 		
 		ui.addSubViewToMain([jum]);
 		
@@ -83,35 +58,210 @@ function loadLandingView() {
 		
 }
 
-//Add Buttons
+
+function fileUploadRow() {
+
+
+  var inpgrp = ui.createElement('div', 'fileupload');
+  inpgrp.setAttribute('class', 'input-group image-preview');
+  var inp1 = ui.createElement('input', '');
+  inp1.setAttribute('type', 'text');
+  inp1.setAttribute('class', 'form-control image-preview-filename');
+  inp1.setAttribute('disabled', 'disabled');
+  inpgrp.appendChild(inp1);
+
+  var spanx = ui.createElement('span', '');
+  spanx.setAttribute('class', 'input-group-btn');
+  inp1.appendChild(spanx);
+
+  var prvbtn = ui.createElement('button', 'prvbtn');
+  prvbtn.setAttribute('class', 'btn btn-default image-preview-clear');
+  prvbtn.setAttribute('style', "display:none;");
+
+
+  var prvIcon = ui.createElement('span', '');
+  prvIcon.setAttribute('class', 'glyphicon glyphicon-remove');
+  prvbtn.appendChild(prvIcon);
+
+  spanx.appendChild(prvbtn);
+
+  var prvinpt = ui.createElement('div', 'img-prv-input');
+  prvinpt.setAttribute('class', 'btn btn-default image-preview-input');
+  var spanf = ui.createElement('span', '');
+  spanf.setAttribute('class', 'glyphicon glyphicon-folder-open');
+  prvinpt.appendChild(spanf);
+
+  var spant = ui.createElement('span', '');
+  spant.setAttribute('class', 'image-preview-input-title');
+  spant.innerText = 'Browse';
+  prvinpt.appendChild(spant);
+
+  var inpt = ui.createElement('input', 'uploadimg');
+  inpt.setAttribute('type', 'file');
+  inpt.setAttribute('accept', 'image/png, image/jpeg, image/gif');
+  inpt.setAttribute('name', 'input-file-preview');
+  prvinpt.appendChild(inpt);
+
+  spanx.appendChild(prvinpt);
+
+
+  //return inpgrp;
+
+   var btn = document.getElementById('buttonrow-col0');
+   btn.innerHTML = '';
+   btn.appendChild(inpgrp);
+
+}
+
 function addButtonCol() {
+
     var upbtn = ui.createElement('a', 'upload');
-    upbtn.setAttribute('class', 'btn btn-block btn-warning');
-    upbtn.innerText = 'Upload Image';
+    upbtn.setAttribute('class', 'btn btn-block btn-warning fileUpload');
+    upbtn.setAttribute('onchange' , 'readFile(this);');
+    var spanx = ui.createElement('span');
+    spanx.innerHTML = 'Upload Image';
+    upbtn.appendChild(spanx);
+
+    var fl = ui.createElement('input', 'imgfile');
+    fl.setAttribute('type', 'file');
+    fl.setAttribute('class', 'upload');
+    fl.setAttribute('name', 'upfile');
+
+    //upbtn.setAttribute('onclick', 'uploadTrigger();');
+    //upbtn.innerText = 'Upload Image';
+    upbtn.appendChild(fl);
 
     var icon = ui.createElement('span', 'uploadicon');
     icon.setAttribute('class', 'glyphicon glyphicon-cloud-upload');
     upbtn.appendChild(icon);
 
-    var formx = ui.createElement('form', 'formidx');
-    formx.setAttribute('action', '/imgupload');
-
     var btn = document.getElementById('buttonrow-col0');
-    var fl = ui.createElement('input', 'imgbtn');
+    var formx = ui.createElement('form', 'imgform');
+    formx.setAttribute('enctype', "multipart/form-data");
+
+    formx.setAttribute('onsubmit', "uploadTrigger();");
+
+    var flupld = ui.createElement('div', 'uploadiv');
+    flupld.setAttribute('class', 'fileUpload btn btn-danger btn-block');
+    var spanx = ui.createElement('span');
+    spanx.innerHTML = 'Upload Image     ';
+    flupld.appendChild(spanx);
+    flupld.appendChild(icon);
+
+    var fl = ui.createElement('input', 'imgfile');
     fl.setAttribute('type', 'file');
+    fl.setAttribute('class', 'upload');
     fl.setAttribute('name', 'upfile');
-    fl.setAttribute('data-url', '/imgupload');
-    formx.appendChild(fl);
+    fl.setAttribute('onchange' , 'readFile(this);');
+    flupld.appendChild(fl);
 
-    btn.appendChild(fl);
-    btn.appendChild(ui.br());
+    formx.appendChild(flupld);
+    formx.appendChild(ui.br());
 
-    btn.appendChild(upbtn);
+    btn.appendChild(formx);
+
+
+
+    //Add Second button which is a 'Download Button'
+    var dnbtn = ui.createElement('a', 'dwnload');
+    dnbtn.setAttribute('class', 'fileUpload btn btn-block btn-info');
+    dnbtn.disabled = true;
+
+    var spanx = ui.createElement('span');
+    spanx.innerHTML = 'Download  ';
+    dnbtn.appendChild(spanx);
+
+    var icon = ui.createElement('span', 'dnloadicon');
+    icon.setAttribute('class', 'glyphicon  glyphicon-save');
+    dnbtn.appendChild(icon);
+
+    var btn = document.getElementById('buttonrow-col1');
+    btn.appendChild(dnbtn);
+
 }
 
-function upload() {
-    console.log('Uploading image..');
+function readFile(input) {
+    console.log('Read File:');
+    if (input.files && input.files[0]) {
+                var reader = new FileReader();
 
+                reader.onload = function (e) {
+                    $('#rawimage').attr('src', e.target.result);
+                    $('#pxlimage').attr('src', e.target.result);
+                    $('#pxlimage').attr('class', 'imcenter imageblur');
+                    uploadTrigger();
+                };
+
+                reader.readAsDataURL(input.files[0]);
+    }
+
+}
+
+function uploadTrigger() {
+    console.log('Upload Trigger');
+    progressModal();
+    $('#appmodal').modal('show');
+
+    var fd = new FormData();
+    var fileSelection = document.getElementById('imgfile');
+    var files = fileSelection.files;
+    var upfile = files[0];
+    fd.append('upfile', upfile, upfile.name);
+
+    for (idx=0; idx < files.length; idx++) {
+        console.log('==> '+ files[idx].name);
+    }
+        $.ajax({
+            url: '/imgupload',
+            type: 'post',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function(response){
+                if(response != 0){
+                    console.log('File uploaded ...');
+                    console.log(response);
+                    updateImages(response);
+                }else{
+                    alert('file not uploaded');
+                }
+            },
+        });
+}
+
+function loadingView() {
+}
+
+function updateImages(msg) {
+    var resp = JSON.parse(msg);
+    $('#appmodal').modal('hide');
+    var urlx = 'http://' + server + '/pixelatefaces?ifile='+resp['upimg'];
+	console.log('Sys: '+ urlx);
+    ajaxLoad(pixelateCallBack, urlx);
+}
+
+function pixelateCallBack(resp) {
+    console.log(resp);
+
+    var rsp = JSON.parse(resp);
+    var outfile = rsp['outfile'];
+    var pxlimg = document.getElementById('pxlimage');
+    pxlimg.setAttribute('src', 'pxltd/'+rsp['outfile']);
+    pxlimage.setAttribute('class', 'imcenter');
+
+    var btnx = document.getElementById('dwnload');
+    btnx.disabled = false;
+    btnx.setAttribute('onclick', 'window.location="http://'+server+'/pxltd/'+rsp['outfile']+'"');
+}
+
+function progressModal() {
+ var mcnt = document.getElementById('modalcontent');
+    mcnt.innerHTML = '';
+    var img = ui.createElement('img', 'pcontent');
+    img.setAttribute('src', 'img/file_uploading.gif');
+    img.setAttribute('class', 'imcenter');
+
+    mcnt.appendChild(img);
 }
 
 function addImageCol() {
@@ -122,8 +272,10 @@ function addImageCol() {
   var attr = col0.getAttribute('class');
   col0.setAttribute('class', attr + ' well');
   col0.appendChild(rawimg);
-
   col0.appendChild(ui.hr());
+
+
+
 
   var pxlimg = ui.createElement('img', 'pxlimage');
   pxlimg.setAttribute('src', 'img/example/abba-1-pxltd.JPG');
@@ -133,18 +285,9 @@ function addImageCol() {
   col1.setAttribute('class', attr + ' well');
   col1.appendChild(pxlimg);
   col1.appendChild(ui.hr());
-
-  var prx = ui.createElement('div', 'progress');
-  col1.appendChild(prx);
 }
 
 
-
-function getSystemInfo() {
-	var urlx = 'http://' + server + '/getsysteminfo'; 
-	console.log('Sys: '+ urlx);
-	ajaxLoad(getsystem_callback, urlx); 
-}
 
 
 
